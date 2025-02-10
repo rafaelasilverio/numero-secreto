026aa511fab5af114a20cb74a65dc3284389cd50
Rafaela Silverio <rafaelapetelin@gmail.com>
2025-02-10 15:14:10 -0300

    first commit

---
+++ app/validacao.js
@@ -0,0 +1,57 @@
+function verificaSeOChutePossuiUmValorValido() {
+
+    const chute = document.getElementById("numeroDigitado").value
+    const elementoChute = document.getElementById('chute')
+
+
+    const numero = +chute
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    } else {
+        elementoChute.innerHTML = `
+        <div class="resposta">Você digitou</div>
+        <span class="box">${chute}</span>
+     `
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar btn-enviar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
+
